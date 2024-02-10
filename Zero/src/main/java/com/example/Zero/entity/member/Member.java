0cc515270ec2c3d74e112097f3fe5a17b5e2edf7
Keonseong <tttzkq9336@gmail.com>
2024-02-10 23:49:16 +0900

    Feat: "엔티티 어노테이션 추가"

--- Zero/src/main/java/com/example/Zero/entity/member/Member.java
+++ Zero/src/main/java/com/example/Zero/entity/member/Member.java
@@ -8,8 +8,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class Member {
 
 	@Id
--- Zero/src/main/java/com/example/Zero/entity/post/Post.java
+++ Zero/src/main/java/com/example/Zero/entity/post/Post.java
@@ -8,8 +8,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class Post {
 
 	@Id
